5ac4d0c5488aa178e973afb205acdbba6173263f
Dustin Quam <dustinquam@me.com>
2017-04-05 10:35:15 -0500

    only saml on live, for now

--- mysite/_config.php
+++ mysite/_config.php
@@ -19,6 +19,6 @@ if (class_exists('SiteTree')) SiteTree::enable_nested_urls();
 
 if(Director::isLive()) {
 	Director::forceSSL();
+	Authenticator::set_default_authenticator('SAMLAuthenticator');
 }
 
-Authenticator::set_default_authenticator('SAMLAuthenticator');
