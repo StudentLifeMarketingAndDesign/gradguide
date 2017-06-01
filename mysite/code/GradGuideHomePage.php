1e7919bcfb8e5fec2be0dde176597a0e0fadd7bc
Dustin Quam <dustinquam@me.com>
2017-06-01 08:59:46 -0500

    topic, header, other changes

--- mysite/code/GradGuideHomePage.php
+++ mysite/code/GradGuideHomePage.php
@@ -1,10 +1,34 @@
 <?php
 class GradGuideHomePage extends Page {
-	
+
+    private static $has_many = array(
+        
+    );	
+
 	public function getPageTypeTheme(){
 		return "dark-header";
 	}
 
+	public function getCMSFields() {
+		$fields=parent::getCMSFields();
+		$fields->removeByName('LayoutType');
+		
+		$conf=GridFieldConfig_RecordViewer::create(20);
+		//$conf->removeComponent('AddNew');
+		$conf->addComponent(new GridFieldSortableRows('GgSortOrder'));
+		
+		$fields->addFieldToTab('Root.Main', new GridField('Topics', 'Feature these topics in the following order', Topic::get(), $conf));
+		
+		return $fields;
+	}
+
+	public function getTopics(){
+		echo 'hello';
+		$topics = Topic::get()->sort('GgSortOrder');
+
+		return $topics;
+
+	}
 }
 
 class GradGuideHomePage_Controller extends Page_Controller {
--- mysite/code/TopicExtension.php
+++ mysite/code/TopicExtension.php
@@ -2,11 +2,14 @@
 
 	class TopicExtension extends DataExtension{
 		private static $db = array(
-
+			'GgSortOrder' => 'Int'
 		);
 
 		private static $belongs_many_many = array (
 
+		);
+		private static $has_one = array(
+			// "GradGuideHomePage" => "GradGuideHomePage",
 		);
 
 		public function updateCMSFields(FieldList $fields){
