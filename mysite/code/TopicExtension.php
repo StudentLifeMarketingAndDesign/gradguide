43f9d92f5e39683a4c85208b52dad8eaa5c8fbaa
Dustin Quam <quamsta@gmail.com>
2017-06-27 11:16:15 -0500

    updated topic

--- mysite/code/TopicExtension.php
+++ mysite/code/TopicExtension.php
@@ -14,6 +14,20 @@
 
 		public function updateCMSFields(FieldList $fields){
 
+			// $locationGridFieldConfig = GridFieldConfig_RelationEditor::create();
+
+			// $locationGridField = GridField::create('LocationPages', 'Relevant locations', $this->owner->LocationPages(), $locationGridFieldConfig);
+
+			// $fields->addFieldToTab('Root.Main', $locationGridField);
+
+		$lField = TagField::create(
+						'LocationPages',
+						'Locations relevant to this topic:',
+						LocationPage::get(),
+						$this->owner->LocationPages()
+					)->setShouldLazyLoad(true)->setCanCreate(false);
+
+		$fields->addFieldToTab('blog-admin-sidebar', $lField);
 		}
 
 
